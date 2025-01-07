e74c5a42a11923dc9eb3a0f4c1f480f852b190e6
sahil <sahilmulani501@gmail.com>
2025-01-07 15:11:43 +0530

    fix favicon and loading

---
+++ frontend/app/loading.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Loading() {
+  return (
+    <div className="flex flex-col w-full items-center justify-center h-screen">
+      <img className="w-[200px] h-[200px]" src="/logo.png" alt="Logo" />
+    </div>
+  );
+}
